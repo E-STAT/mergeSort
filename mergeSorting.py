e621efdeadecfe8ca9611c1293467f25877f8fd1
E-STAT <owojori.tolulope@gmail.com>
2020-10-23 13:03:29 +0100

    merge sort

---
+++ mergeSorting.py
@@ -0,0 +1,42 @@
+def mergeSort(arr):
+    
+    #calculate separtor med
+    if len(arr) > 1:
+        mid = len(arr)//2
+
+        left = arr[:mid]
+        right = arr[mid:]
+        mergeSort(left)
+        mergeSort(right)
+
+
+        #iterators for the 2 separated arrays
+        i = 0
+        j = 0
+
+        #iterator for the combined array
+        k = 0
+
+        while i<len(left) and j<len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[j] = right[j]
+                j += 1
+            k +=1
+
+        #for the remianing
+        while i<len(left):
+            arr[k] = left[i]
+            i +=1
+            k +=1
+        while j<len(right):
+            arr[k] = right[j]
+
+            j +=1
+            k +=1
+
+myList = [54,26,93,17,77,31,44,55,20]
+mergeSort(myList)
+print(myList)
---
+++ recursion.py
@@ -0,0 +1,26 @@
+# def sumIntegers(x):
+
+#     if x == 0:
+#         return 0
+#     return sumIntegers(x-1)+x
+
+# print(sumIntegers(5))
+
+
+# def reverseList(array):
+
+#     if array == []:
+#         return []
+    
+
+#     else:
+#         first = array[0:1]
+#         others = reverseList(array[1:])
+#         reversed = others + first
+
+#     return reversed
+
+# print(reverseList([1,2,3,4]))
+
+print(5/2)
+print(5//2)
